2a3c67b1ae6b9e4702f2ab5682ad022ab4beba38
Mrush07 <148041712+Mrush07@users.noreply.github.com>
2025-12-18 15:54:25 +0100

    removing api key

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.get("/scorers", async (req, res) => {
   try {
     const response = await fetch("https://api.football-data.org/v4/competitions/SA/scorers", {
-      headers: { "X-Auth-Token": "f8bc98d037144f6193cfc95f91679c99" }
+      headers: { "X-Auth-Token": process.env.FOOTBALL_API_KEY }
     });
     const data = await response.json();
     res.json(data);
